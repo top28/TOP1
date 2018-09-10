52aa95d492f1ad375491a88e18c8d6b791e196e8
top28 <43137170+top28@users.noreply.github.com>
2018-09-10 18:16:53 +0700

    Add files via upload

---
+++ TOP TASK 1/src/top/task/pkg1/TOPTASK1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package top.task.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class TOPTASK1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int jumlah = 10;
+            System.out.print("jumlah=");
+            System.out.print(jumlah);
+        for(int a=0;a<=jumlah;a++){
+           for(int b=0;b<a;b++){
+               System.out.print("*");
+           } 
+           System.out.println("");
+}
+    }
+    
+}
